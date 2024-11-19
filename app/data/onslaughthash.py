0dd9edee94e2b3d68ad30fde83569a05b058f9d3
Kurt Anderson <akurt6805@protonmail.com>
2024-11-18 19:02:59 -0800

    Add S24 Onslaught IDs. Ignore venv folders.

--- app/data/onslaughthash.py
+++ app/data/onslaughthash.py
@@ -1,5 +1,5 @@
-
 ONSLAUGHT_ACTIVITIES = {
+    # original onslaught activity IDS
     2769402771: 'Onslaught: Playlist',
 
     203968370: 'Midtown',
@@ -12,5 +12,29 @@ ONSLAUGHT_ACTIVITIES = {
     
     1428607109: 'Vostok',
     3439345485: 'Vostok: Onslaught',
-    264092439: 'Legend: Vostok: Onslaught'
+    264092439: 'Legend: Vostok: Onslaught',
+
+
+    # s24 onslaught activities added
+    647593269: 'Onslaught: Playlist',
+    1310338947: 'Onslaught: Playlist',
+
+    1785542986: 'Eventide Ruins: Onslaught: Salvation',
+    1345669408: 'Expert: Eventide Ruins: Onslaught: Salvation',
+
+    2617136386: 'Kell\'s Grave: Onslaught: Salvation',
+    655113048: 'Expert: Kell\'s Grave: Onslaught: Salvation',
+
+    1852884874: 'Mothyards: Onslaught',
+    952872983: 'Expert: Mothyards: Onslaught',
+
+    1852174165: 'Midtown: Onslaught',
+    2060744492: 'Expert: Midtown: Onslaught',
+
+    676763021: 'Vostok: Onslaught',
+    3733482932: 'Expert: Vostok: Onslaught',
+
+    1618784039: 'Widow\'s Court: Onslaught: Salvation',
+    1543235317: 'Expert: Widow\'s Court: Onslaught: Salvation'
+
     }
